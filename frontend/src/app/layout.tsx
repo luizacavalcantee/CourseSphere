defa91c497fbcba9ac154c057d44b5ace37d2111
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-07-13 13:37:27 -0300

    feat(auth): implement AuthContext and AuthProvider for user authentication management

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,10 @@
+import { AuthProvider } from '@/contexts/AuthContext';
+import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
 
-import 'styles/globals.css';
+import '@/styles/globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'CourseSphere',
@@ -8,22 +12,22 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icons/icon.png",
-        type: "image/svg+xml",
+        url: '/icons/icon.svg',
+        type: 'image/svg+xml',
       },
     ],
   },
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
-      <body>
-        {children}
+    <html lang="pt-br">
+      <body className={inter.className}>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import React, { createContext, useState, useContext, ReactNode, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  user: User | null;
+  login: (userData: User) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = (userData: User) => {
+    setUser(userData);
+    localStorage.setItem('user', JSON.stringify(userData));
+    router.push('/');
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('user');
+    router.push('/login');
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated: !!user, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth deve ser usado dentro de um AuthProvider');
+  }
+  return context;
+};
