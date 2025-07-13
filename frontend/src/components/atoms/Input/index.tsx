323a6a5205dba0255b210cedc5a1eee205cd43bf
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-07-13 13:51:09 -0300

    feat(input): create reusable Input component with customizable classes

---
+++ frontend/src/components/atoms/Input/index.tsx
@@ -0,0 +1,14 @@
+import { InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = ({ className, ...props }: InputProps) => {
+  const baseClasses = 
+    "w-full pt-4 pb-2 bg-transparent border-b-2 border-gray-300 focus:outline-none focus:border-indigo-500 transition-colors";
+  
+  const combinedClasses = `${baseClasses} ${className}`;
+
+  return (
+    <input className={combinedClasses} {...props} />
+  );
+};
