b46d8ba6d20e5fffe4100eeec761508ca5233de9
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-07-14 11:48:39 -0300

    feat(button): simplify button styles by removing focus ring classes

--- frontend/src/components/atoms/Button/index.tsx
+++ frontend/src/components/atoms/Button/index.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const Button = ({ children, className, ...props }: ButtonProps) => {
     const baseClasses =
-        'w-full px-4 py-3 font-semibold text-white uppercase rounded-md hover:bg-primaryHover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors';
+        'w-full px-4 py-3 font-semibold text-white uppercase rounded-md hover:bg-primaryHover focus:outline-none transition-colors';
     const combinedClasses = `${baseClasses} ${className}`;
 
     return (
